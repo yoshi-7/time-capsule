0d695e6ae57adb6db00e11748de1e60386c9e2ef
AurÃelien Pirus <a.pirus@hotmail.fr>
2024-02-17 15:50:25 +0100

    added lock partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_17_115324) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_17_143600) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_17_115324) do
     t.datetime "updated_at", null: false
     t.string "status", default: "draft"
     t.string "name"
+    t.date "unlock_date"
     t.index ["user_id"], name: "index_capsules_on_user_id"
   end
 
