b0af385cfd7ca5e6435a07b007d23e7fd331c25d
jeremy da silva <jeremydasilva1@icloud.com>
2024-02-17 11:17:21 +0100

    users and capsules in db/seeds.rb added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user1 = User.create!(email: 'dev1@example.com', password: 'password')
+user2 = User.create!(email: 'dev2@example.com', password: 'password')
+user3 = User.create!(email: 'dev3@example.com', password: 'password')
+user4 = User.create!(email: 'dev4@example.com', password: 'password')
+
+3.times do |i|
+  Capsule.create!(name: "Capsule #{i + 1}", user: user1)
+end
+
+2.times do |i|
+  Capsule.create!(name: "Capsule #{i + 1}", user: user2)
+end
+
+4.times do |i|
+  Capsule.create!(name: "Capsule #{i + 1}", user: user3)
+end
+
+2.times do |i|
+  Capsule.create!(name: "Capsule #{i + 1}", user: user4)
+end
