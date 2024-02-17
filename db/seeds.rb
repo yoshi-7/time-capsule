46630f82931931667aa3161d73926e830b23bf69
AurÃelien Pirus <a.pirus@hotmail.fr>
2024-02-17 14:17:09 +0100

    added draft-partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,7 @@
 User.create!(email: 'dev1@example.com', password: 'password', first_name: 'User', last_name: 'One')
-User.create!(email: 'dev2@example.com', password: 'password', first_name: 'User', last_name: 'Tow')
-User.create!(email: 'dev3@example.com', password: 'password', first_name: 'User', last_name: 'Three')
-User.create!(email: 'dev4@example.com', password: 'password', first_name: 'User', last_name: 'Four')
 
-User.each do |user|
-  types = ['draft', 'unlocked', 'locked']
+statuses = ['draft', 'unlocked', 'locked']
 
-  types.each_with_index do |type, i|
-    Capsule.create!(name: "Capsule #{i + 1}", user: user, status: type)
-  end
+6.times do
+  Capsule.create!(name: "Capsule name", user: User.first, status: statuses.sample)
 end
