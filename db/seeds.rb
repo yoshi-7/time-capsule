61d89c97e86395813205d29e3e0904087111a6bc
AurÃelien Pirus <a.pirus@hotmail.fr>
2024-02-22 21:35:11 +0100

    implemented nav links to test features

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,9 @@ User.create!(email: 'dev1@example.com', password: 'password', first_name: 'User'
 statuses = ['draft', 'unlocked', 'locked']
 
 6.times do
-  Capsule.create!(name: "Capsule name", user: User.first, status: statuses.sample, encapsulation_date: Time.now.to_date, unlock_date: rand(2.years).seconds.from_now )
+  Capsule.create!(name: "Capsule name", user: User.first, status: statuses.sample, encapsulation_date: Time.now.to_date, unlock_date: rand(2.years).seconds.from_now)
+end
+
+3.times do
+  Capsule.create!(name: "Capsule name", user: User.last, status: statuses.sample, encapsulation_date: Time.now.to_date, unlock_date: rand(1.year.ago..Time.now))
 end
