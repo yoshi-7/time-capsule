2d347c3d04a0fc8d59e05bde33370fa690bf2b86
AurÃelien Pirus <a.pirus@hotmail.fr>
2024-02-24 12:25:10 +0100

    tab-switch-ready

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,14 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
+  static targets = ['capsule', 'tabBtn']
   connect() {
     console.log('toggle')
   }
 
   toggleDisplay() {
-    // Hide all capsules
-    this.capsuleTarget.forEach(capsule => {
+    this.capsuleTargets.forEach(capsule => {
+      console.log(capsule)
       capsule.classList.toggle("d-none");
     });
+    this.tabBtnTargets.forEach(target => {
+      target.classList.toggle("tab-active");
+    })
   }
 }
