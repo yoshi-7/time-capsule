71edddb18d09282e19e490887c9a0446f8b61b17
Guillaume CHARLIER <guillaume.charlier@cfecgc-inova.fr>
2024-02-29 20:46:11 +0100

    finishing backend show opened capsule but need to upload media content to test it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,6 @@ User.create!(email: 'dev1@example.com', password: 'password', first_name: 'User'
 end
 
 3.times do
-  Capsule.create!(name: "Capsule name", user: User.last, status: 'unlocked', encapsulation_date: Time.now.to_date, unlock_date: rand(1.year.ago..Time.now))
+  Capsule.create!(name: "Capsule name", user: User.last, status: 'unlocked', encapsulation_date: Time.now.to_date, unlock_date: rand(1.year.ago..Time.now), messages: 2.times do
+    Message.new(title: "Message title", content: "Message content"))
 end
