137715c3c381aea76c5a8db314276f1e2621fe29
Guillaume CHARLIER <guillaume.charlier@cfecgc-inova.fr>
2024-03-01 10:12:54 +0100

    landing page set up

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@ end
 
 3.times do
   Capsule.create!(name: "Capsule name", user: User.last, status: 'unlocked', encapsulation_date: Time.now.to_date, unlock_date: rand(1.year.ago..Time.now), messages: 2.times do
-    Message.new(title: "Message title", content: "Message content"))
+    Message.new(title: "Message title", content: "Message content")end)
 end
