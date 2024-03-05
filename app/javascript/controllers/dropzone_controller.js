cbb8e719911fd79bc804aa4f15107a727801cf2f
AurÃelien Pirus <a.pirus@hotmail.fr>
2024-03-05 20:12:52 +0100

    change import path from dropzone controller

--- app/javascript/controllers/dropzone_controller.js
+++ app/javascript/controllers/dropzone_controller.js
@@ -6,7 +6,7 @@ import {
   findElement,
   removeElement,
   insertAfter
-} from "/helpers";
+} from "/app/javascript/helpers";
 
 Dropzone.autoDiscover = false; // necessary quirk for Dropzone error in console
 
