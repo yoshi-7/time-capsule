10af0b951b35130eb00ea8eddd3ca6cb872d6974
AurÃelien Pirus <a.pirus@hotmail.fr>
2024-03-05 22:18:59 +0100

    work on autosave for photos form & send capsule feature

--- app/controllers/user_capsules_controller.rb
+++ app/controllers/user_capsules_controller.rb
@@ -1,6 +1,13 @@
 class UserCapsulesController < ApplicationController
   def create
-    raise
-
+    @capsule = params[:capsule_id]
+    @recipients = params[:recipients]
+    @recipients.each do |recipient|
+      UserCapsule.new(capsule: @capsule, user: User.create(email: recipient))
+    end
+    @capsule.update(encapsulation_date: Time.now(), status: 'locked')
   end
+  raise
+
+  private
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  validates :first_name, :last_name, presence: true
+  validates :email, presence: true
 
   def name
     "#{first_name} #{last_name}"
