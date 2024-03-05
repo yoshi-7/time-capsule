207b9f7023267092f49728736a9fb49ba4d50dbb
AurÃelien Pirus <a.pirus@hotmail.fr>
2024-03-05 19:45:23 +0100

    added eagerload on controller/index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,10 @@
 
 import { application } from "./application"
 
+// Eager load all controllers defined in the import map under controllers/**/*_controller
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
+
 import AutosaveController from "./autosave_controller"
 application.register("autosave", AutosaveController)
 
