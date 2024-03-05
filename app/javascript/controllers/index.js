45a4aa33d11a3f4ac1eea39ee1313e5f276c606d
AurÃelien Pirus <a.pirus@hotmail.fr>
2024-03-05 19:57:20 +0100

    change import path for application.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,7 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
