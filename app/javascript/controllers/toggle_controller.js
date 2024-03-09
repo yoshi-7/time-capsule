1b3703a6332fc990b67360eeadd20b61e415e950
Simon Inazuma <yoshimune2017@gmail.com>
2024-03-09 10:49:18 +0100

    toggle now added to photos, videos and audios

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["capsule", "tabBtn", "message", "toggleIcon"]
+  static targets = ["capsule", "tabBtn", "message", "photo", "video", "audio", "toggleIcon"]
 
   connect() {
     console.log('Toggle controller connected')
@@ -20,6 +20,29 @@ export default class extends Controller {
   toggleMessages() {
     this.messageTargets.forEach(message => {
       message.classList.toggle("d-none")
+    })
+    this.toggleIconTarget.style.transform = 'rotate(180deg)'
+  }
+
+  togglePhotos() {
+    this.photoTargets.forEach(photo => {
+      photo.classList.toggle("d-none")
+    })
+    this.toggleIconTarget.style.transform = 'rotate(180deg)'
+  }
+
+
+  toggleVideos() {
+    this.videoTargets.forEach(video => {
+      video.classList.toggle("d-none")
+    })
+    this.toggleIconTarget.style.transform = 'rotate(180deg)'
+  }
+
+
+  toggleAudios() {
+    this.audioTargets.forEach(audio => {
+      audio.classList.toggle("d-none")
     })
     this.toggleIconTarget.style.transform = 'rotate(180deg)'
   }
