94ca9a3f7c4ec8e8716903c4834966783453845e
Simon Inazuma <yoshimune2017@gmail.com>
2024-03-09 14:26:54 +0100

    html setup for layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resources)
     capsules_path
   end
-end
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
+end
